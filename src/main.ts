73c86f6bda63199e34485f5850027145922ca3e0
werwer748 <werwer748@naver.com>
2022-12-21 23:46:43 +0900

    nestjs 처음해본다 ㄷㄷ

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`서버 ${port}PORT에서 실행중`);
+}
+bootstrap();
